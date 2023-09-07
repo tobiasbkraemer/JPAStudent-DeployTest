6c9a5f1388cf7c8c7ecbd6cd3e588aa929b90bac
erlm <erlm@kea.dk>
2023-09-07 13:54:08 +0200

    nu med hej

--- src/main/java/com/example/jpastudent/repositories/UnicodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpastudent.repositories;
-
-import com.example.jpastudent.model.Unicode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UnicodeRepository extends JpaRepository<Unicode, Integer> {
-}
---
+++ src/test/java/com/example/jpastudent/repositories/StudentRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.jpastudent.repositories;
+
+import com.example.jpastudent.model.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class StudentRepositoryTest {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @BeforeEach
+    void setUp() {
+        Student std = new Student();
+        std.setName("test");
+        studentRepository.save(std);
+    }
+
+    @Test
+    void testOneStudent() {
+
+        List<Student> lst =  studentRepository.findAll();
+        assertEquals(1, lst.size());
+    }
+
+
+}
